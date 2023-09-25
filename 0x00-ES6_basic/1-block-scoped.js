9a6ae373b519570a588a1d3689bdef1dd808e654
Michael Hudson <mnkotagu@gmail.com>
2023-09-25 14:53:06 +0300

    Improve 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
     /* eslint-disable-next-line no-unused-vars */
-    let task = true;
+    const task = true;
     /* eslint-disable-next-line no-unused-vars */
-    let task2 = false;
+    const task2 = false;
   }
 
   return [task, task2];
