e7c6a31988f21e533ce905b99423a898fc4b0fed
Michael Hudson <mnkotagu@gmail.com>
2023-09-25 16:06:39 +0300

    Implement 10-loops.js

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const appendedArray = [];
+
+  for (const value of array) {
+    appendedArray.push(appendString + value);
+  }
+
+  return appendedArray;
+}
