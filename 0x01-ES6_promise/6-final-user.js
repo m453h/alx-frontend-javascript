4e6dc17a0f5cf4602d2ecb5f5c1ec1fa8a6c25af
Michael Hudson <mnkotagu@gmail.com>
2023-09-26 21:21:01 +0300

    Make further improvements to 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  let signUpResponse;
   let uploadPhotoResponse;
 
-  try {
-    signUpResponse = await signUpUser(firstName, lastName);
-  } catch (error) {
-    signUpResponse = { status: 'rejected', value: error.toString() };
-  }
+  const signUpResponse = await signUpUser(firstName, lastName);
 
   try {
     uploadPhotoResponse = await uploadPhoto(fileName);
