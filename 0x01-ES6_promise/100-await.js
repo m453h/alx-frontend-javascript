6f707f6f37281b0cb83b5fe567e7c8e5e6c69c33
Michael Hudson <mnkotagu@gmail.com>
2023-09-26 20:50:00 +0300

    Implement 100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default async function asyncUploadUser() {
+  let response = {};
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    response = { photo, user };
+  } catch (error) {
+    response = { photo: null, user: null };
+  }
+  return response;
+}
