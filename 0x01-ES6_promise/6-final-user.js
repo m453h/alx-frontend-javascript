5e2bb042f41bb39c58bb4aa0586d646463d897ce
Michael Hudson <mnkotagu@gmail.com>
2023-09-26 20:06:37 +0300

    Improve 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,16 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
-    status: 'fulfilled',
-    value: result,
-  }))).catch((error) => [
-    {
-      status: 'rejected',
-      value: error,
-    },
-  ]);
+  return signUpUser(firstName, lastName)
+    .then((userData) => uploadPhoto(fileName)
+      .then((fileData) => [
+        { status: 'fulfilled', value: userData },
+        { status: 'fulfilled', value: fileData },
+      ])
+      .catch((error) => [{ status: 'rejected', value: error.toString() }]))
+    .catch((error) => [{ status: 'rejected', value: error.toString() }]);
 }
