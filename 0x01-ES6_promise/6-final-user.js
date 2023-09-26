cb2f4b93f89aed9a77938af86b6943a5085816fe
Michael Hudson <mnkotagu@gmail.com>
2023-09-26 21:34:27 +0300

    Improve 6-final-user.js to include proper response format

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,14 +3,11 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   let uploadPhotoResponse;
-
-  const signUpResponse = await signUpUser(firstName, lastName);
-
+  const signUpResponse = { status: 'fulfilled', value: await signUpUser(firstName, lastName) };
   try {
     uploadPhotoResponse = await uploadPhoto(fileName);
   } catch (error) {
     uploadPhotoResponse = { status: 'rejected', value: error.toString() };
   }
-
   return [signUpResponse, uploadPhotoResponse];
 }
