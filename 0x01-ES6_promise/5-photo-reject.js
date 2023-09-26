48ef476e56abacdd6e2f2b18460ee7275e1676cd
Michael Hudson <mnkotagu@gmail.com>
2023-09-26 19:41:06 +0300

    Implement 5-photo-reject.js

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,6 @@
+export default function uploadPhoto(filename) {
+  const promise = new Promise(((resolve, reject) => {
+    reject(new Error(`${filename} cannot be processed`));
+  }));
+  return promise;
+}
