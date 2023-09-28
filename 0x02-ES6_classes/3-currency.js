9e240013318e57f0c06e1ab8977a04ca0105ee3d
Michael Hudson <mnkotagu@gmail.com>
2023-09-28 14:42:19 +0300

    Implement 3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,43 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('TypeError: Name must be a string');
+    }
+
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new Error('TypeError: Code must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new Error('TypeError: Name must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new Error('TypeError: Code must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
