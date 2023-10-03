87758dcc918e3305683eb63bf269ba888c6c68de
Michael Hudson <mnkotagu@gmail.com>
2023-10-03 12:39:59 +0300

    Implement 5-typed_arrays.js

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+export default function createInt8TypedArray(length, position, value) {
+  const arrayBuffer = new ArrayBuffer(length);
+  const view = new DataView(arrayBuffer);
+
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+
+  return view;
+}
