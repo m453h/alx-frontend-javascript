886108ef1b665bdf9e69912a84ddada06147790b
Michael Hudson <mnkotagu@gmail.com>
2023-10-03 12:27:37 +0300

    Improve 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,5 @@
 export default function updateStudentGradeByCity(studentList, city, newGrades) {
   if (Array.isArray(studentList)) {
-
     return studentList.filter((student) => student.location === city)
       .map((student) => ({ ...student, grade: (newGrades.filter((grade) => grade.studentId === student.id)[0] || { grade: 'N/A' }).grade }));
   }
