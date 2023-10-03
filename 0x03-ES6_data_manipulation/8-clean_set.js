a7296c4d569f5e99fb5c42ff0671c87cc0a20b11
Michael Hudson <mnkotagu@gmail.com>
2023-10-03 14:44:24 +0300

    Add null handler for 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   if (startString && typeof startString === 'string') {
     const startsWith = Array.from(set)
-      .filter((value) => value.startsWith(startString))
+      .filter((value) => value && value.startsWith(startString))
       .map((value) => value.substring(startString.length));
 
     return startsWith.join('-');
