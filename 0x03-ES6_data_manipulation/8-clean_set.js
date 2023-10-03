6f30e4df4b1d44c40cdbe43142d6f5166255317b
Michael Hudson <mnkotagu@gmail.com>
2023-10-03 14:27:44 +0300

    Improve 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 export default function cleanSet(set, startString) {
-  const startsWith = Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.substring(startString.length));
+  if (startString !== '') {
+    const startsWith = Array.from(set)
+      .filter((value) => value.startsWith(startString))
+      .map((value) => value.substring(startString.length));
 
-  return startsWith.join('-');
+    return startsWith.join('-');
+  }
+  return '';
 }
