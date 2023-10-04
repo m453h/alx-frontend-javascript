e59db084ee1042f08dd5e5f8663fcdef7ad7c619
Michael Hudson <mnkotagu@gmail.com>
2023-10-04 21:42:22 +0300

    Implement Task 6 solution

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -50,7 +50,14 @@ function createEmployee(salary: number | string) {
   }
 }
 
-// Example usage
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
