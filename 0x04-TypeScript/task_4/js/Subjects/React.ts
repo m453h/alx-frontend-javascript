12b20de14f1e4ebc75ce8c8c87dcf24ff3da2c79
Michael Hudson <mnkotagu@gmail.com>
2023-10-04 23:03:04 +0300

    Implement Task 9 solution

---
+++ 0x04-TypeScript/task_4/js/Subjects/React.ts
@@ -0,0 +1,25 @@
+/// <reference path="Teacher.ts" />
+/// <reference path="Subject.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+
+  export class React extends Subject {
+
+    getRequirements(): string {
+      return "Here is the list of requirements for React";
+    }
+
+    getAvailableTeacher(): string {
+      
+      if (this.teacher && this.teacher.experienceTeachingC !== undefined) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      } else {
+        return "No available teacher";
+      }
+    }
+  }
+}
