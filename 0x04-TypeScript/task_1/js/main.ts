29e67a5fd37d0730d924062007190bf98d55c852
Michael Hudson <mnkotagu@gmail.com>
2023-10-05 03:47:24 +0300

    Improve task_1 interface exports

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,9 +1,9 @@
-interface Teacher {
+export interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
-  readonly location: string;
+  location: string;
   [key: string]: any;
 }
 
@@ -19,11 +19,11 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName[0]}. ${lastName}`;
 };
 
-interface StudentConstructor {
+export interface StudentConstructor {
   new (firstName: string, lastName: string): StudentClassInterface;
 }
 
-interface StudentClassInterface {
+export interface StudentClassInterface {
   workOnHomework(): string;
   displayName(): string;
 }
